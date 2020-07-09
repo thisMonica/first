60debba92b960efadd5ef934e276f744a3e7b42f
hujiahan_2 <hujiahan@evrazlabs.email>
2020-07-09 15:50:53 +0800

    普通算法题

---
+++ src/main/java/com/example/first/algorithm/Demo1.java
@@ -0,0 +1,38 @@
+package com.example.first.algorithm;
+
+/**
+ * @author hu
+ * @date 2020/7/9 11:17
+ */
+public class Demo1 {
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] ints = twoSum(nums, target);
+        System.out.println(ints);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    System.out.println(nums[i] + "+" + nums[j]);
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
