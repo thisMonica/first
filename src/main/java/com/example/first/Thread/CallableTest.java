1a4b33fb3b4eba901e2cc6bc5234173588b84dd3
hujiahan_2 <hujiahan@evrazlabs.email>
2020-07-09 09:56:52 +0800

    callable创建线程

--- src/main/java/com/example/first/Thread/CallableTest.java
+++ src/main/java/com/example/first/Thread/CallableTest.java
@@ -1,17 +1,30 @@
 package com.example.first.Thread;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @author hu
  * @date 2020/5/12 12:31
  */
 public class CallableTest {
 
-    public static void main(String[] args) {
-        int a = 0;
-        assert a == 0;
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
+        MyCallbale myCallbale = new MyCallbale();
+        FutureTask futureTask = new FutureTask(myCallbale);
+        new Thread(futureTask,"thread1").start();
+        String  callable = (String) futureTask.get();
+        System.out.println(callable);
     }
 
+    static class MyCallbale implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            return "hello callable";
+        }
+    }
 
 }
 
